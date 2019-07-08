09b4f0f2f1260a436d9f98ac79d22b41e8b14931
Lucius Mathametica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-08 02:54:10 +0100

    Get name of item

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ $( document ).ready(function() {
 
 	$( ".result" ).html( "Let's get some data!" );
 
-	$.get( "https://www.mcdonalds.com/wws/json/getItemDetails.htm?country=UK&item=200145", function( data ) {
+	$.get( "https://www.mcdonalds.com/wws/json/getItemDetails.htm?country=UK&item=200145", 
+	function( data ){
 
 	  console.log( "Load was performed." );
 
+	  $( ".result" ).html(data.item.imported_item_name);
 	  console.log( data );
+	  //["attach_item_hero_image"]["description"]["image_name"]
 	
 	})
     .fail(function(xhr, textStatus, errorThrown) {
